6705fa4d5411e00be378ca7e4723d85b5702dc68
Frederic Branca <frederic.branca@gmail.com>
2023-09-12 14:42:15 +0200

    FormType du guide gérer les entrées du formulaire

--- src/Form/GuideType.php
+++ src/Form/GuideType.php
@@ -7,14 +7,19 @@ use App\Entity\DataChampion;
 use Symfony\Component\Form\AbstractType;
 use App\Validator\Constraints\AllowedVoies;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class GuideType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // Récupère de la liste des champions passée en option lors de la création du formulaire
+        $champions = $options['champions'];
+
         $builder
             ->add('titre', TextType::class, [
                 'required' => true,
@@ -36,13 +41,16 @@ class GuideType extends AbstractType
                 'attr' => [
                     'autocomplete' => 'off'
                 ],
-                'constraints' => [
-                    new AllowedVoies(['allowedVoies'=> ['Mid', 'Top', 'Jungle', 'Bottom', 'Support']])
-                ]
             ])
-            // ->add('champion', DataChampion::class, [
-            //     'required' => true
-            // ])
+            ->add('champion', EntityType::class, [
+                'required' => true,
+                'class' => DataChampion::class,
+                'choices' => $champions
+                ]
+            )
+            ->add('Valider', SubmitType::class, [
+                'label' => 'Publier le guide'
+            ])
         ;
     }
 
@@ -50,6 +58,7 @@ class GuideType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Guide::class,
+            'champions' => [] // initialisation de la liste dans un tableau vide pour éviter les erreurs
         ]);
     }
 }
