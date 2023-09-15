9c8a7ac4e5a76884d949033d34dc1de622fbe7a7
Frederic Branca <frederic.branca@gmail.com>
2023-09-15 17:01:50 +0200

    update db

--- db.sql
+++ db.sql
@@ -21,7 +21,7 @@ USE `guidelol`;
 
 -- Listage de la structure de table guidelol. data_champion
 CREATE TABLE IF NOT EXISTS `data_champion` (
-  `id` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -202,37 +202,35 @@ CREATE TABLE IF NOT EXISTS `data_sort_invocateur` (
 
 -- Listage de la structure de table guidelol. doctrine_migration_versions
 CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
-  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `version` varchar(191) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `executed_at` datetime DEFAULT NULL,
   `execution_time` int DEFAULT NULL,
   PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- Listage des données de la table guidelol.doctrine_migration_versions : ~1 rows (environ)
-INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-	('DoctrineMigrations\\Version20230913143803', '2023-09-13 16:38:09', 228);
+-- Listage des données de la table guidelol.doctrine_migration_versions : ~0 rows (environ)
 
 -- Listage de la structure de table guidelol. guide
 CREATE TABLE IF NOT EXISTS `guide` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `champion_id` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `titre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `voie` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `champion` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `titre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `voie` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `modified_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
-  PRIMARY KEY (`id`),
-  KEY `IDX_CA9EC735FA7FD7EB` (`champion_id`),
-  CONSTRAINT `FK_CA9EC735FA7FD7EB` FOREIGN KEY (`champion_id`) REFERENCES `data_champion` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table guidelol.guide : ~0 rows (environ)
+-- Listage des données de la table guidelol.guide : ~1 rows (environ)
+INSERT INTO `guide` (`id`, `champion`, `titre`, `voie`, `created_at`, `modified_at`) VALUES
+	(9, 'Ahri', 'test', 'Top', '2023-09-15 14:54:36', NULL);
 
 -- Listage de la structure de table guidelol. messenger_messages
 CREATE TABLE IF NOT EXISTS `messenger_messages` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
@@ -248,8 +246,8 @@ CREATE TABLE IF NOT EXISTS `messenger_messages` (
 CREATE TABLE IF NOT EXISTS `sort_invocateur` (
   `id` int NOT NULL AUTO_INCREMENT,
   `guide_id` int NOT NULL,
-  `titre` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `commentaire` longtext COLLATE utf8mb4_unicode_ci,
+  `titre` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `commentaire` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `ordre` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_CEBB8376D7ED1D4B` (`guide_id`),
@@ -274,9 +272,9 @@ CREATE TABLE IF NOT EXISTS `sort_invocateur_data_sort_invocateur` (
 -- Listage de la structure de table guidelol. user
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(180) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `roles` json NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `is_verified` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`)
