8c095087e53b055a51b3cffa45cf797eb19d927c
Frederic Branca <frederic.branca@gmail.com>
2023-09-23 23:37:47 +0200

    Ajout de ChampionService et méthodes

---
+++ src/Service/ChampionService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+
+use App\HttpClient\LoLHttpClient;
+
+class ChampionService {
+    private $lol;
+    private $imgUrl;
+
+    public function __construct(LoLHttpClient $lol, string $imgUrl) {
+        $this->lol = $lol;
+        $this->imgUrl = $imgUrl;
+    }
+
+    public function getChampions() {
+        $championsData = $this->lol->getChampions();
+        $championsData = json_decode($championsData, true)['data']['champions'];
+        $champions = [];
+        foreach ($championsData as $champion) {
+            $key = $champion['idChamp'];
+            $champions[$key] = $champion;
+        }
+        return $champions;
+    }
+
+    public function getChampionsIdName() {
+        $championsData = $this->lol->getChampions();
+        $champions = json_decode($championsData, true)['data']['champions'];
+        $championsName = [];
+        foreach ($champions as $key => $champion) {
+            $championsIdName[$champion['name']] = $champion['idChamp'];
+        }
+        return $championsIdName;
+    }
+
+    public function getChampionImageURL() {
+        return $this->imgUrl;
+    }
+}
