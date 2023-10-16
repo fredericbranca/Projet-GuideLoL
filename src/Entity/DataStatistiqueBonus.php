f5f61d500780bc92fb1bd41c7d8d7f417bf066f2
Frederic Branca <frederic.branca@gmail.com>
2023-10-17 01:21:48 +0200

    Création entité des bonus supplémentaires (runes)

---
+++ src/Entity/DataStatistiqueBonus.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DataStatistiqueBonusRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DataStatistiqueBonusRepository::class)]
+class DataStatistiqueBonus
+{
+    #[ORM\Id]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $bonus_value = null;
+
+    #[ORM\Column]
+    private ?int $bonus_line = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getBonusValue(): ?string
+    {
+        return $this->bonus_value;
+    }
+
+    public function setBonusValue(string $bonus_value): static
+    {
+        $this->bonus_value = $bonus_value;
+
+        return $this;
+    }
+
+    public function getBonusLine(): ?int
+    {
+        return $this->bonus_line;
+    }
+
+    public function setBonusLine(int $bonus_line): static
+    {
+        $this->bonus_line = $bonus_line;
+
+        return $this;
+    }
+}
