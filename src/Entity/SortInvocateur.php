66bbe40a5288dc1a7df69e020b6be4945ee70cf9
Frederic Branca <frederic.branca@gmail.com>
2023-10-22 22:41:15 +0200

    Correction : format camelCase

--- src/Entity/SortInvocateur.php
+++ src/Entity/SortInvocateur.php
@@ -29,7 +29,7 @@ class SortInvocateur
     #[ORM\JoinTable(name: "choix_sort_invocateur")]
     private Collection $choixSortInvocateur;
 
-    #[ORM\ManyToOne(inversedBy: 'GroupeSortsInvocateur')]
+    #[ORM\ManyToOne(inversedBy: 'groupeSortsInvocateur')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Guide $guide = null;
 
