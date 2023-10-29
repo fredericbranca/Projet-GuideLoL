b8086bdcf07007430ad5f1d99d9281e3c47fed24
Frederic Branca <frederic.branca@gmail.com>
2023-10-29 23:33:57 +0100

    Requete pour trier le form avec l'arbre et type

--- src/Repository/DataRuneRepository.php
+++ src/Repository/DataRuneRepository.php
@@ -21,28 +21,39 @@ class DataRuneRepository extends ServiceEntityRepository
         parent::__construct($registry, DataRune::class);
     }
 
-//    /**
-//     * @return DataRune[] Returns an array of DataRune objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('d')
-//            ->andWhere('d.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('d.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findByArbreAndType(string $arbre, string $type): array
+    {
+        return $this->createQueryBuilder('dr')
+            ->where('dr.runeArbre = :arbre')
+            ->andWhere('dr.runeType = :type')
+            ->setParameter('arbre', $arbre)
+            ->setParameter('type', $type)
+            ->getQuery()
+            ->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?DataRune
-//    {
-//        return $this->createQueryBuilder('d')
-//            ->andWhere('d.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    /**
+    //     * @return DataRune[] Returns an array of DataRune objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('d')
+    //            ->andWhere('d.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('d.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?DataRune
+    //    {
+    //        return $this->createQueryBuilder('d')
+    //            ->andWhere('d.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
