1d221c8261cbfc1232b74cf25e2f0163238c1cd1
Frederic Branca <frederic.branca@gmail.com>
2023-10-30 21:28:18 +0100

    Fct pour gérer l'affichage des arbres de runes

--- public/js/guide.js
+++ public/js/guide.js
@@ -61,6 +61,14 @@ async function fetchContainer(spanId) {
     }
 }
 
+// Fonction pour reset les boutons radio d'un élément
+function resetRadioButtons(element) {
+    let radios = element.querySelectorAll('input[type="radio"]');
+    for (let radio of radios) {
+        radio.checked = false;
+    }
+}
+
 // -----------------------
 // ÉCOUTEURS D'ÉVÉNEMENTS
 // -----------------------
@@ -113,5 +121,92 @@ menu.addEventListener("click", function (event) {
             document.querySelector(mappingBuilderMenu[id]).style.display = "block";
             fetchContainer(id);
         }
+    }
+});
+
+
+// Ajout d'un écouteur d'événement click sur les arbres des runes
+let firstClickedTree = null;
+let secondClickedTree = null;
+
+document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('arbre')) {
+        let runesOptions = event.target.nextElementSibling;
+
+        // Si on click sur le premier arbre alors qu'un 2ème est ouvert
+        if (event.target === firstClickedTree && secondClickedTree) {
+            secondClickedTree.nextElementSibling.style.display = 'none'; // Ferme le deuxième arbre ouvert
+            resetRadioButtons(secondClickedTree.nextElementSibling);
+            let firstDivSecondTree = secondClickedTree.nextElementSibling.querySelector('div');
+            if (firstDivSecondTree) {
+                firstDivSecondTree.style.display = 'block'; // Réinitialise le deuxième arbre
+            }
+            runesOptions.style.display = 'none'; // Ferme le premier arbre
+            resetRadioButtons(runesOptions);
+            let firstDivFirstTree = runesOptions.querySelector('div');
+            if (firstDivFirstTree) {
+                firstDivFirstTree.style.display = 'block'; // Réinitialise le premier arbre
+            }
+            firstClickedTree = null;
+            secondClickedTree = null;
+            return; // Sort de l'écouteur d'événements
+        }
+
+        // Si aucun arbre n'a été cliké ou si on reclick sur le premier arbre
+        if (!firstClickedTree || firstClickedTree === event.target) {
+            if (runesOptions.style.display === 'block') {
+                resetRadioButtons(runesOptions);
+                runesOptions.style.display = 'none';
+                let firstDiv = runesOptions.querySelector('div');
+                if (firstDiv) {
+                    firstDiv.style.display = 'block';
+                }
+                firstClickedTree = null;
+            } else {
+                runesOptions.style.display = 'block';
+                firstClickedTree = event.target;
+            }
+            secondClickedTree = null;
+        }
+        // Si on click sur un deuxième arbre différent du premier et que le premier a déjà été défini
+        else if (firstClickedTree && !secondClickedTree && event.target !== firstClickedTree) {
+            runesOptions.style.display = 'block';
+            let firstDiv = runesOptions.querySelector('div');
+            if (firstDiv) {
+                firstDiv.style.display = 'none';
+            }
+
+            secondClickedTree = event.target;
+
+            // Désactive les événements de clic pour tous les autres arbres
+            let trees = document.querySelectorAll('.arbre');
+            for (let tree of trees) {
+                if (tree !== firstClickedTree && tree !== secondClickedTree) {
+                    tree.removeEventListener('click', arguments.callee);
+                }
+            }
+        }
+        // Si on reclick sur le deuxième arbre 
+        else if (secondClickedTree === event.target) {
+            resetRadioButtons(runesOptions);
+            runesOptions.style.display = 'none';
+            let firstDiv = runesOptions.querySelector('div');
+            if (firstDiv) {
+                firstDiv.style.display = 'block';
+            }
+            secondClickedTree = null;
+        }
+    }
+});
+
+// Ajout d'un écouteur d'événement sur le span Publier pour qu'il active le click sur le bouton qui valide le formulaire
+document.addEventListener("DOMContentLoaded", function () {
+    let actionPublierButton = document.getElementById('action-publier');
+    let guideValiderButton = document.getElementById('guide_Valider');
+
+    if (actionPublierButton && guideValiderButton) {
+        actionPublierButton.addEventListener('click', function () {
+            guideValiderButton.click();
+        });
     }
 });
