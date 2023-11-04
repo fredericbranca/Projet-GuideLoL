d98b9f1625b710539fc1f460972b2ecb49ae8fd2
Frederic Branca <frederic.branca@gmail.com>
2023-11-04 14:22:34 +0100

    f° runesHelper pour enregistrer les runes

--- src/Service/GuideService.php
+++ src/Service/GuideService.php
@@ -2,77 +2,42 @@
 
 namespace App\Service;
 
-use App\Entity\Guide;
-use App\Entity\RunesPage;
-use App\Entity\SortInvocateur;
 use App\Entity\AssociationsRunesBonus;
 use App\Entity\AssociationsArbresRunes;
-use Doctrine\ORM\EntityManagerInterface;
 
 class GuideService
 {
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
+    public function runesHelper($form, $guide, $runesPage, $groupesRunes, $entityManager)
     {
-        $this->entityManager = $entityManager;
-    }
-
-    public function createGuideFromForm($formData, $champion, $groupesSortsInvocateur, $groupesRunes, $runesData)
-    {
-        // Première partie du guide
-        $guide = new Guide();
-        $guide->setTitre($formData->getTitre());
-        $guide->setVoie($formData->getVoie());
-        $guide->setChampion($champion);
-        // Persist et flush le guide pour obtenir son ID
-        $this->entityManager->persist($guide);
-        $this->entityManager->flush();
-
-        // Sorts d'invocateur
-        foreach ($groupesSortsInvocateur as $groupeSortsInvocateur) {
-            $sortInvocateur = new SortInvocateur;
-            $sortInvocateur->setGuide($guide);
-            $sortInvocateur->setTitre($groupeSortsInvocateur->getTitre());
-            $sortInvocateur->setCommentaire($groupeSortsInvocateur->getCommentaire());
-            $sortInvocateur->setOrdre($groupeSortsInvocateur->getOrdre());
-            $spells = $groupeSortsInvocateur->getChoixSortInvocateur();
-            foreach ($spells as $spell) {
-                $sortInvocateur->addChoixSortInvocateur($spell);
-            }
-            $this->entityManager->persist($sortInvocateur);
-        }
-
-        // Runes
-        foreach ($groupesRunes as $groupeRunes) {
-            $runesPage = new RunesPage();
-            $runesPage->setGuide($guide);
+        foreach ($groupesRunes as $key => $groupeRunes) {
             $runesPage->setTitre($groupeRunes->getTitre());
             $runesPage->setCommentaire($groupeRunes->getCommentaire());
             $runesPage->setOrdre($groupeRunes->getOrdre());
 
-            // Persiste et flush le groupe de runes pour obtenir son ID
-            $this->entityManager->persist($runesPage);
-            $this->entityManager->flush();
+            // Récupère les objet data runes
+            $arbres = ['Domination', 'Inspiration', 'Precision', 'Resolve', 'Sorcery'];
+            $types = ['Primary', 'Secondary1', 'Secondary2', 'Secondary3'];
 
-            foreach ($runesData as $rune) {
-                $associationArbresRunes = new AssociationsArbresRunes();
-                $associationArbresRunes->setType('test');
-                $associationArbresRunes->addChoixRune($rune);
-                $this->entityManager->persist($associationArbresRunes);
-                $this->entityManager->flush();
+            foreach ($arbres as $arbre) {
+                foreach ($types as $type) {
+                    $dataRune = $form->get('groupeRunes')->get($key)->get($arbre)->get($type)->getData();
+                    if ($dataRune) {
+                        $associationArbresRunes = new AssociationsArbresRunes();
+                        $associationArbresRunes->setType('test');
+                        $associationArbresRunes->addChoixRune($dataRune);
+                        $entityManager->persist($associationArbresRunes);
 
-                $associationRunesBonus = new AssociationsRunesBonus();
-                $associationRunesBonus->setType('Rune');
-                $associationRunesBonus->addChoixArbre($associationArbresRunes);
-                $this->entityManager->persist($associationRunesBonus);
-                $this->entityManager->flush();
+                        $associationRunesBonus = new AssociationsRunesBonus();
+                        $associationRunesBonus->setType('Rune');
+                        $associationRunesBonus->addChoixArbre($associationArbresRunes);
+                        $entityManager->persist($associationRunesBonus);
 
-                $runesPage->addChoixRunesPages($associationRunesBonus);
+                        $runesPage->addChoixRunesPages($associationRunesBonus);
+                    }
+                }
             }
-            $this->entityManager->persist($runesPage);
+            $entityManager->persist($runesPage);
+            $guide->addGroupeRune($runesPage);
         }
-
-        $this->entityManager->flush();
     }
 }
