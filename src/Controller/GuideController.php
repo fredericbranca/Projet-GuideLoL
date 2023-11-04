e4622247e20ddbbe51f7ed2714987f809e7d12dc
Frederic Branca <frederic.branca@gmail.com>
2023-11-04 14:21:52 +0100

    f° runesHelper pour enregistrer les runes

--- src/Controller/GuideController.php
+++ src/Controller/GuideController.php
@@ -52,45 +52,16 @@ class GuideController extends AbstractController
             $groupesRunes = $form->get('groupeRunes')->getData();
 
             if (!$groupesRunes->isEmpty()) {
-                foreach ($groupesRunes as $key => $groupeRunes) {
-                    $runesPage->setTitre($groupeRunes->getTitre());
-                    $runesPage->setCommentaire($groupeRunes->getCommentaire());
-                    $runesPage->setOrdre($groupeRunes->getOrdre());
-
-                    // Récupère les objet data runes
-                    $arbres = ['Domination', 'Inspiration', 'Precision', 'Resolve', 'Sorcery'];
-                    $types = ['Primary', 'Secondary1', 'Secondary2', 'Secondary3'];
-
-                    foreach ($arbres as $arbre) {
-                        foreach ($types as $type) {
-                            $dataRune = $form->get('groupeRunes')->get($key)->get($arbre)->get($type)->getData();
-                            if ($dataRune) {
-                                $associationArbresRunes = new AssociationsArbresRunes();
-                                $associationArbresRunes->setType('test');
-                                $associationArbresRunes->addChoixRune($dataRune);
-                                $entityManager->persist($associationArbresRunes);
-
-                                $associationRunesBonus = new AssociationsRunesBonus();
-                                $associationRunesBonus->setType('Rune');
-                                $associationRunesBonus->addChoixArbre($associationArbresRunes);
-                                $entityManager->persist($associationRunesBonus);
-
-                                $runesPage->addChoixRunesPages($associationRunesBonus);
-                            }
-                        }
-                    }
-                    $entityManager->persist($runesPage);
-                    $guide->addGroupeRune($runesPage);
-                }
+                // Ajout des runes dans la DB
+                $guideService->runesHelper($form, $guide, $runesPage, $groupesRunes, $entityManager);
             }
 
-            // Ajout des runes dans la DB
-            // $guideService->createGuideFromForm($guide, $groupesRunes, $runesData);
 
             $entityManager->persist($guide);
             $entityManager->flush();
 
             return $this->redirectToRoute('new_guide');
+            
         } else if ($form->isSubmitted() && !$form->isValid()) {
             // Si le formulaire est invalide
             $errors = []; // Tableau pour stocker les erreurs
