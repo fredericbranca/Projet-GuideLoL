b0c1f6149b0813916e132fdfb773e63a007ca916
Frederic Branca <frederic.branca@gmail.com>
2023-11-06 21:06:47 +0100

    Fonction pour entrer les compétences dans la db

--- src/Service/GuideService.php
+++ src/Service/GuideService.php
@@ -2,8 +2,10 @@
 
 namespace App\Service;
 
+use App\Entity\DataCompetence;
 use App\Entity\AssociationsRunesBonus;
 use App\Entity\AssociationsArbresRunes;
+use App\Entity\AssociationsCompetencesNiveaux;
 
 class GuideService
 {
@@ -42,7 +44,7 @@ class GuideService
             $bonusLines = [1, 2, 3];
 
             foreach ($bonusLines as $line) {
-                $dataBonus = $form->get('groupeRunes')->get($key)->get('bonusLine'.$line)->getData();
+                $dataBonus = $form->get('groupeRunes')->get($key)->get('bonusLine' . $line)->getData();
                 if ($dataBonus) {
                     $associationRunesBonus = new AssociationsRunesBonus();
                     $associationRunesBonus->setType('Bonus');
@@ -55,6 +57,34 @@ class GuideService
 
             $entityManager->persist($runesPage);
             $guide->addGroupeRune($runesPage);
+        }
+    }
+
+    public function competencesHelper($guide, $formCompetences, $groupesCompetences, $groupesCompetencesData, $entityManager)
+    {
+        $repository = $entityManager->getRepository(DataCompetence::class);
+
+        foreach ($groupesCompetences as $key => $groupeCompetence) {
+            $formCompetences->setTitre($groupeCompetence->getTitre());
+            $formCompetences->setCommentaire($groupeCompetence->getCommentaire());
+            $formCompetences->setOrdre($groupeCompetence->getOrdre());
+
+            $competencesData = $groupesCompetencesData[$key]['competence'];
+
+            foreach ($competencesData as $id => $competenceData) {
+                $dataCompetence = $repository->findOneBy(['id' => $id]);
+
+                if ($dataCompetence) {
+                    $associationsCompetencesNiveaux = new AssociationsCompetencesNiveaux();
+                    $associationsCompetencesNiveaux->setNiveaux($competenceData);
+                    $associationsCompetencesNiveaux->addCompetence($dataCompetence);
+                    $entityManager->persist($associationsCompetencesNiveaux);
+
+                    $formCompetences->addChoixCompetencesNiveau($associationsCompetencesNiveaux);
+                }
+            }
+
+            $guide->addGroupesCompetence($formCompetences);
         }
     }
 }
