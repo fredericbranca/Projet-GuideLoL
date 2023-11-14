1c66f568b17b2f45856c6897ce0b65c3d299ab85
Frederic Branca <frederic.branca@gmail.com>
2023-11-14 23:21:43 +0100

    Route pour renvoyer un mail de vérification

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,6 +8,7 @@ use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
 use Symfony\Component\Mime\Address;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
@@ -78,7 +80,7 @@ class RegistrationController extends AbstractController
             $tokenStorage->setToken($token);
             $session->set('_security_main', serialize($token));
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_profile');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -107,12 +109,54 @@ class RegistrationController extends AbstractController
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
 
-            return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('app_profile');
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Votre adresse e-mail a été vérifiée.');
 
         return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/verify/newEmail', name: 'app_verify_newEmail')]
+    public function verifyNewEmail(Security $security, SessionInterface $session)
+    {
+        $user = $security->getUser();
+
+        if ($user->isVerified()) {
+            throw new AccessDeniedException('Action non autorisé.');
+        }
+
+        $lastEmailSentTime = $session->get('last_email_sent_time');
+
+        // Vérifier si le délai de refroidissement est écoulé (30mn)
+        if ($lastEmailSentTime && (time() - $lastEmailSentTime) < 1800) {
+            $tempsRestant = 1800 - (time() - $lastEmailSentTime);
+            $minutesRestante = ceil($tempsRestant / 60);
+            $this->addFlash('note', 'Veuillez patienter ' . $minutesRestante . ' minutes avant de renvoyer un email.');
+            return $this->redirectToRoute('app_profile');
+        }
+
+        // generate a signed url and email it to the user
+        $this->emailVerifier->sendEmailConfirmation(
+            'app_verify_email',
+            $user,
+            (new TemplatedEmail())
+                ->from(new Address('admin@example.com', 'GuidesLoL Mail bot'))
+                ->to($user->getEmail())
+                ->subject('Veuillez confirmer votre Email')
+                ->htmlTemplate('registration/confirmation_email.html.twig')
+        );
+
+        // Enregistrer l'heure actuelle comme dernière heure d'envoi
+        $session->set('last_email_sent_time', time());
+
+        // Ajouter un message de réussite
+        $this->addFlash(
+            'note', // Type du message (success, error, warning, etc.)
+            'Un email de vérification a été envoyé. Veuillez vérifier votre boîte de réception.' // Message
+        );
+
+        return $this->redirectToRoute('app_profile');
     }
 }
