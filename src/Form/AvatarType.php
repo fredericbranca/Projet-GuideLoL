3848d0053c25dd3e0dd4307f4a948c9eb26b6d93
Frederic Branca <frederic.branca@gmail.com>
2023-11-15 00:51:35 +0100

    Avatar Form Type

---
+++ src/Form/AvatarType.php
@@ -0,0 +1,30 @@
+<?php
+
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Image;
+
+class AvatarType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('avatar', FileType::class, [
+                'label' => 'Avatar (format image)',
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez télécharger une image valide',
+                    ])
+                ]
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Modifier l\'avatar']);
+    }
+}
