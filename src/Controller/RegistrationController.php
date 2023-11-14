8175196bd68ae6b21a9e4f8d9d4c7170bbd1a142
Frederic Branca <frederic.branca@gmail.com>
2023-11-14 16:14:02 +0100

    Redirection si déjà connecté

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -38,6 +38,10 @@ class RegistrationController extends AbstractController
         TokenStorageInterface $tokenStorage,
         SessionInterface $session
     ): Response {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
