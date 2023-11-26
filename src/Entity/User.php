4d7934df584accb7d800db51a9ed642e8f24b716
Frederic Branca <frederic.branca@gmail.com>
2023-11-26 13:45:20 +0100

    Ajout __toString pseudo pour l'user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -196,4 +196,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString() {
+        return $this->getPseudo();
+    }
 }
