801d44640963730e23ce90250631f2d3d344cc46
Frederic Branca <frederic.branca@gmail.com>
2023-11-29 20:49:59 +0100

    Ajout de l'url des images à home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\ChampionService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ChampionService $championService): Response
     {
+        $img_url = $championService->getChampionImageURL();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'img_url' => $img_url,
         ]);
     }
 }
