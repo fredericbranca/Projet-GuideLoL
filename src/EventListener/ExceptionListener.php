73051a7f917cef257837ace6198e9c5e0e729607
Frederic Branca <frederic.branca@gmail.com>
2023-11-29 11:16:46 +0100

    ajout error404

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Twig\Environment;
+
+class ExceptionListener
+{
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        // Récupère l'exception
+        $exception = $event->getThrowable();
+
+        // Gère uniquement les exceptions NotFoundHttpException
+        if ($exception instanceof NotFoundHttpException) {
+            $response = new Response();
+            $response->setContent($this->twig->render('bundles/TwigBundle/Exception/error404.html.twig'));
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+
+            $event->setResponse($response);
+        }
+    }
+}
