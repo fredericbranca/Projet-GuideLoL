94ff5ffe454c8f4f40ded31fc4764a6b8a70eddf
Frederic Branca <frederic.branca@gmail.com>
2023-11-29 20:49:02 +0100

    fetch les champion avec l'url vers le guide filtré

--- public/js/searchChampions.js
+++ public/js/searchChampions.js
@@ -1,11 +1,11 @@
-const searchInput = document.getElementById('guide-filtre-champion');
+const searchInput = document.getElementById('search-champion');
 
 // Script pour rechercher des champions et afficher le résultat
 document.addEventListener('DOMContentLoaded', function () {
     searchInput.addEventListener('input', function () {
         var searchTerm = searchInput.value;
-        // Span qui affiche les résultats
-        var span = document.querySelector('.search-result');
+        // div qui affiche les résultats
+        var div = document.querySelector('.search-result');
 
         if (searchTerm !== "") {
 
@@ -15,20 +15,19 @@ document.addEventListener('DOMContentLoaded', function () {
                 })
                 .then(function (data) {
                     // Efface la recherche précédente
-                    span.innerHTML = '';
+                    div.innerHTML = '';
 
                     // Ajoute chaque champion
                     // Ecouteur d'évènement sur un champion pour valider le form avec le filtre du champion
                     Object.keys(data.champions).forEach(function (championKey) {
                         var champion = data.champions[championKey];
-                        var div = document.createElement('div');
-                        div.textContent = champion.name;
-                        div.classList.add('champion-div');
-                        div.addEventListener('click', function () {
-                            document.getElementById('guide_filtre_champion').value = champion.name; // Met à jour la valeur du champ
-                            document.getElementById('form-guide-filter').submit(); // Soumet le formulaire
-                        });
-                        span.appendChild(div);
+                        div.innerHTML += `
+                            <a class="champion-link" href="/guides?champion=${champion.name}">
+                                <div class="image-wrapper">
+                                    <img src="${url}/champion/${champion.idChamp}.png.webp" alt="${champion.name}">
+                                </div>
+                                <div class="champion-name">${champion.name}</div>
+                            </a>`;
                     });
                 })
                 .catch(function (error) {
@@ -37,44 +36,20 @@ document.addEventListener('DOMContentLoaded', function () {
                 });
 
         } else {
-            span.innerHTML = '';
-            span.appendChild();
+            div.innerHTML = '';
         }
     });
 });
 
-// Execute le formulaire au clic sur un role
-divRoles = document.getElementById('guide_filtre_role');
+// Ecouteur d'év sur les scroll pour éviter que la propagation ce fasse sur les éléments parent
+document.getElementById('scrollable-container').addEventListener('wheel', function (event) {
+    var element = event.currentTarget;
+    var scrollTop = element.scrollTop;
+    var scrollHeight = element.scrollHeight;
+    var height = element.clientHeight;
+    var delta = event.deltaY;
 
-divRoles.addEventListener('click', function (event) {
-    // Vérifie si l'élément cliqué est un label
-    if (event.target.tagName === 'LABEL') {
-        // Temporisation pour permettre à l'input radio d'être coché
-        setTimeout(function () {
-
-            document.getElementById('form-guide-filter').submit();
-        }, 10);
+    if ((delta > 0 && scrollTop + height >= scrollHeight) || (delta < 0 && scrollTop <= 0)) {
+        event.preventDefault();
     }
-});
-
-
-// Efface le filtre des champions
-spanClearChampion = document.getElementById('clear-champion-btn');
-spanClearChampion.addEventListener('click', function () {
-    document.getElementById('guide_filtre_champion').value = '';
-    document.getElementById('form-guide-filter').submit();
-});
-
-// Affiche le nom du champion actuel dans le champ recherche
-let hiddenInputChamp = document.getElementById('guide_filtre_champion');
-if (hiddenInputChamp.value) {
-    searchInput.value = hiddenInputChamp.value;
-}
-
-// Efface tout le formulaire
-spanClear = document.getElementById('clear-btn');
-spanClear.addEventListener('click', function () {
-    document.getElementById('guide_filtre_champion').value = '';
-    document.getElementById('guide_filtre_role_placeholder').checked = 'checked';
-    document.getElementById('form-guide-filter').submit();
 });
