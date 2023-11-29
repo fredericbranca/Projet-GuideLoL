5c453ce8bdab11855a0c25018d0981e886a5b26f
Frederic Branca <frederic.branca@gmail.com>
2023-11-29 17:25:27 +0100

    GuideFiltreType -> HiddenType au lieu d'un search

--- src/Form/GuideFiltreType.php
+++ src/Form/GuideFiltreType.php
@@ -4,26 +4,23 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class GuideFiltreType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('champion', SearchType::class, [
+            ->add('champion', HiddenType::class, [
                 'required' => false,
-                'label' => 'Champion',
-                'attr' => [
-                    'class' => 'label-titre'
-                ]
             ])
             ->add('role', ChoiceType::class, [
                 'required' => false,
                 'multiple' => false,
                 'expanded' => true,
+                'placeholder' => 'Tout',
                 'choices' => [
                     'Mid' => 'Mid',
                     'Jungle' => 'Jungle',
