99d6951b843238729f173751e81ab1a9371a1744
Frederic Branca <frederic.branca@gmail.com>
2023-12-03 01:13:45 +0100

    Création entité Evaluation

---
+++ src/Entity/Evaluation.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EvaluationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EvaluationRepository::class)]
+class Evaluation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $notation = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $commentaire = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\ManyToOne(inversedBy: 'evaluations')]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(inversedBy: 'evaluations')]
+    private ?Guide $guide = null;
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTimeImmutable(); // Permet de mettre la date de creation à created_at lors de la création de l'objet
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNotation(): ?int
+    {
+        return $this->notation;
+    }
+
+    public function setNotation(?int $notation): static
+    {
+        $this->notation = $notation;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(?string $commentaire): static
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getGuide(): ?Guide
+    {
+        return $this->guide;
+    }
+
+    public function setGuide(?Guide $guide): static
+    {
+        $this->guide = $guide;
+
+        return $this;
+    }
+}
