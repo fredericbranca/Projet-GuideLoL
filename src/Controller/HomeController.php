2bf936b0f0adaccf87371f8452deae37e63d7291
Frederic Branca <frederic.branca@gmail.com>
2023-12-09 21:21:34 +0100

    Route pour effacer le cookie remember me

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use App\Service\ChampionService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -17,5 +18,17 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'img_url' => $img_url,
         ]);
+    }
+
+    // Route pour effacer le cookie remember me
+    #[Route('/clearRememberme', name: 'app_rememberme', methods: ['POST'])]
+    public function clearRememberme(Request $request)
+    {
+        $response = new Response();
+        if ($request->cookies->has('REMEMBERME')) {
+            $response->headers->clearCookie('REMEMBERME');
+        }
+
+        return $response;
     }
 }
