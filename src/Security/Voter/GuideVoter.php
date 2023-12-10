018f6cf71df12419f8e6845633bffa9f82c4c714
Frederic Branca <frederic.branca@gmail.com>
2023-12-10 02:12:52 +0100

    Ajout autorisation pour noter un guide dans le Voter

--- src/Security/Voter/GuideVoter.php
+++ src/Security/Voter/GuideVoter.php
@@ -6,19 +6,20 @@ use App\Entity\User;
 use App\Entity\Guide;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class GuideVoter extends Voter
 {
     public const GUIDE_EDIT = 'guide_edit';
-    
     public const GUIDE_DELETE = 'guide_delete';
+    public const GUIDE_NOTE = 'guide_note';
 
     protected function supports(string $attribute, mixed $guide): bool
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, [self::GUIDE_EDIT, self::GUIDE_DELETE])
+        return in_array($attribute, [self::GUIDE_EDIT, self::GUIDE_DELETE, self::GUIDE_NOTE])
             && $guide instanceof \App\Entity\Guide;
     }
 
@@ -31,7 +32,7 @@ class GuideVoter extends Voter
         }
 
         // On vérifie si le guide a un user
-        if(null === $guide->getUser()) {
+        if (null === $guide->getUser()) {
             return $this->canCreate($guide, $user);
         }
 
@@ -43,23 +44,50 @@ class GuideVoter extends Voter
             case self::GUIDE_DELETE:
                 // on vérifie si on peut supprimer
                 return $this->canDelete($guide, $user);
+            case self::GUIDE_NOTE:
+                // on vérifie s'il peut noter le guide
+                return $this->canNote($guide, $user);
         }
 
         return false;
     }
 
-    private function canCreate(Guide $guide, User $user) {
+    private function canCreate(Guide $guide, User $user)
+    {
         // L'utilisateur peut créer un guide
         return true;
     }
 
-    private function canEdit(Guide $guide, User $user) {
+    private function canEdit(Guide $guide, User $user)
+    {
         // Le propriétaire du guide peut le modifier
         return $user === $guide->getUser();
     }
 
-    private function canDelete(Guide $guide, User $user) {
+    private function canDelete(Guide $guide, User $user)
+    {
         // Le propriétaire du guide peut le supprimer
         return $user === $guide->getUser();
+    }
+
+    private function canNote(Guide $guide, User $user)
+    {
+        // L'utilisateur ne peut noter son propre guide
+        if ($user === $guide->getUser()) {
+            throw new AccessDeniedException("Vous ne pouvez pas noter votre propre guide");
+        }
+
+        // Si un utilisateur a déjà voté sur un gudie return false
+        $evaluations = $guide->getEvaluations();
+        foreach ($evaluations as $evaluation) {
+            if ($evaluation->getNotation() !== null) {
+                if ($evaluation->getUser() === $user) {
+                    return false;
+                }
+            }
+        }
+
+        // Sinon return true, il peut noter ce guide
+        return true;
     }
 }
