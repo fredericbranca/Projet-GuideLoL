529a8a218d1b871994cb76a7922a9d8fbebdc5ab
Frederic Branca <frederic.branca@gmail.com>
2023-12-10 02:11:55 +0100

    Ajout redirection personnalisé après erreur

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -17,7 +17,15 @@ class AccessDeniedHandler implements AccessDeniedHandlerInterface
 
     public function handle(Request $request, AccessDeniedException $accessDeniedException): RedirectResponse
     {
-        $request->getSession()->getFlashBag()->add('error', $accessDeniedException->getMessage());
+        $errorMessage = $accessDeniedException->getMessage();
+        $request->getSession()->getFlashBag()->add('error', $errorMessage);
+
+        if ($errorMessage === "Vous ne pouvez pas noter votre propre guide" || $errorMessage === "Vous avez déjà voté") {
+            // Redirige erreur formulaire notation (depuis le Voter)
+            $id = $request->attributes->get('_route_params')['id'];
+
+            return new RedirectResponse($this->urlGenerator->generate('get_guide_byId', ['id' => $id]));
+        }
 
         return new RedirectResponse($this->urlGenerator->generate('app_login'));
     }
