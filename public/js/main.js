6d09524055cd0227aa158bfcbe0ae034a8e29aea
Frederic Branca <frederic.branca@gmail.com>
2023-12-11 02:20:12 +0100

    Message tempo après suppression de compte

--- public/js/main.js
+++ public/js/main.js
@@ -16,3 +16,16 @@ function displayMessage() {
 displayMessage();
 
 
+// Message temporaire
+document.addEventListener('DOMContentLoaded', (event) => {
+    let messageCookie = document.cookie.split('; ').find(row => row.startsWith('messagetempo='));
+    if (messageCookie) {
+        let message = decodeURIComponent(messageCookie.split('=')[1]);
+        const element = document.createElement('div');
+        element.classList.add('flash-message', 'flash-success', 'active');
+        element.textContent = message;
+        document.querySelector('.content').appendChild(element);
+        displayMessage();
+        document.cookie = "messagetempo=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;";
+    }
+});
