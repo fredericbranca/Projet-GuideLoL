65260a6091516ef45110fc5cef9bd6fa49627762
Frederic Branca <frederic.branca@gmail.com>
2023-12-11 23:23:31 +0100

    Ajout de contenu depuis l'API et db pour la home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\EvaluationRepository;
+use App\Repository\GuideRepository;
 use App\Service\ChampionService;
+use App\Service\ItemService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +14,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ChampionService $championService): Response
-    {
+    public function index(
+        ChampionService $championService,
+        EvaluationRepository $evaluationRepository,
+        ItemService $itemService
+    ): Response {
+        // URL des images
         $img_url = $championService->getChampionImageURL();
+        // Dernier Champion
+        $lastChampion = $championService->getChampion('Briar');
+
+        // Listes des 10 guides les mieux notés
+        $bestGuides = $evaluationRepository->getBestGuides();
+        // Les derniers commentaires
+        $commentaires = $evaluationRepository->getDerniersCommentaires();
+        // Liste des items
+        $itemsList = $itemService->getItems();
 
         return $this->render('home/index.html.twig', [
             'img_url' => $img_url,
+            'best_guides' => $bestGuides,
+            'commentaires' => $commentaires,
+            'list_items' => $itemsList,
+            'last_champ' => $lastChampion
         ]);
     }
 
